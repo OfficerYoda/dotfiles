28fa20b59dc20cbf11d0ab0522a6d5deca807847
Liam Verzano <l.verzano@sap.com>
2026-01-27 12:42:21 +0100

    Automatic sync: 2026-01-27 12:42:21

---
+++ dot_config/nvim/lua/plugins/code-companion.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    -- Load only when you use these commands
+    cmd = { "CodeCompanion", "CodeCompanionChat", "CodeCompanionActions" },
+    opts = {
+      strategies = {
+        chat = { adapter = "ollama" },
+        inline = { adapter = "ollama" },
+      },
+      adapters = {
+        ollama = function()
+          return require("codecompanion.adapters").extend("ollama", {
+            schema = {
+              model = {
+                default = "qwen3-coder:30b",
+              },
+            },
+          })
+        end,
+      },
+    },
+  },
+}
