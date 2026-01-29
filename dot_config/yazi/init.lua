9f67930cb04c295f901fe3eb8e6daa438cffb5c9
Liam Verzano <l.verzano@sap.com>
2026-01-29 20:03:17 +0100

    Automatic sync: 2026-01-29 20:03:17

---
+++ dot_config/yazi/init.lua
@@ -0,0 +1,7 @@
+-- ya pkg add yazi-rs/plugins:no-status
+-- from: https://github.com/yazi-rs/plugins/tree/main/no-status.yazi
+require("no-status"):setup()
+
+-- ya pkg add yazi-rs/plugins:full-border
+-- from: https://github.com/yazi-rs/plugins/tree/main/full-border.yazi
+require("full-border"):setup()
