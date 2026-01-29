f5c88f8aa4dcc31dae264031cb67b2a98890714c
OfficerYoda <officeryoda@gmail.com>
2026-01-29 18:34:51 +0100

    Automatic sync: 2026-01-29 18:34:51

--- sync-all.fish
+++ sync-all.fish
@@ -1,8 +1,14 @@
-#!/opt/homebrew/bin/fish
+#!/usr/bin/env fish
 
-set SCRIPT_DIR /Users/I767799/.local/share/chezmoi
-set PYTHON /Users/I767799/.local/share/mise/installs/python/3.11.14/bin/python3
-set FISH /opt/homebrew/bin/fish
+if test (uname) = Darwin
+    set SCRIPT_DIR /Users/I767799/.local/share/chezmoi
+    set PYTHON /Users/I767799/.local/share/mise/installs/python/3.11.14/bin/python3
+    set FISH /opt/homebrew/bin/fish
+else
+    set SCRIPT_DIR /home/officeryoda/.local/share/chezmoi
+    set PYTHON /usr/bin/python3
+    set FISH /usr/bin/fish
+end
 
 echo "Syncing dotfiles with chezmoi..."
 $PYTHON $SCRIPT_DIR/sync-chezmoi.py
