6d3abdf260efddaa82f6a1f9ac4a57624488d794
Liam Verzano <l.verzano@sap.com>
2026-02-06 09:36:35 +0100

    Automatic sync: 2026-02-06 09:36:35

--- dot_config/nvim/lua/plugins/smear-cursor.lua
+++ dot_config/nvim/lua/plugins/smear-cursor.lua
@@ -4,8 +4,11 @@ return {
     event = "VeryLazy", -- Only load after the UI is ready
     opts = {
       stiffness = 0.8,
-      trailing_stiffness = 0.79,
-      never_draw_over_target = false,
+      trailing_stiffness = 0.5,
+      -- distance_stop_animating = 0.5,
+
+      -- trailing_stiffness = 0.79,
+      -- never_draw_over_target = false,
     },
   },
 }
