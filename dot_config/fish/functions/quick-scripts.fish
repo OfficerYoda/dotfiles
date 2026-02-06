716907f68e126a3bb9b53bc88d5007fc58e938cc
OfficerYoda <officeryoda@gmail.com>
2026-02-06 15:25:10 +0100

    Automatic sync: 2026-02-06 15:25:10

--- dot_config/fish/functions/quick-scripts.fish
+++ dot_config/fish/functions/quick-scripts.fish
@@ -2,7 +2,7 @@ function quick-scripts
     set all_scripts (fd . ~/.local/bin --max-depth 1 --type f --type l --extension fish --exec basename)
 
     set selected_scripts (printf "%s\n" $all_scripts |
-        fzf-tmux -p 55%,60% --reverse --no-preview --header "Select script(s) to run" --multi)
+        fzf-tmux -w 55% -h 60% --reverse --no-preview --header "Select script(s) to run" --multi)
 
     if test -n "$selected_scripts"
         # for script in $selected_scripts
