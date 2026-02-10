7faef027202376997440efdcc149aa1173c051b6
Liam Verzano <l.verzano@sap.com>
2026-02-10 08:36:05 +0100

    Automatic sync: 2026-02-10 08:36:05

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -1,8 +1,7 @@
 return {
   "obsidian-nvim/obsidian.nvim",
   version = "*",
-  -- cond = vim.fn.getcwd():find(vim.fn.resolve(vim.fn.expand("~/Documents/vault")), 1, true) == 1,
-  cond = true,
+  cond = vim.fn.getcwd():find(vim.fn.resolve(vim.fn.expand("~/Documents/vault")), 1, true) == 1,
   dependencies = {
     "nvim-lua/plenary.nvim",
     "saghen/blink.cmp",
