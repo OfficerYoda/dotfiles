c57748c0c96c11337cfb767f0570f5bd11de95bd
Liam Verzano <l.verzano@sap.com>
2026-02-10 14:55:29 +0100

    Automatic sync: 2026-02-10 14:55:29

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -4,7 +4,7 @@ return {
   opts = {
     -- 'default' for mappings: 'presets', 'super-tab' or 'enter'
     keymap = {
-      preset = "enter",
+      -- preset = "enter",
       ["<Tab>"] = {
         function(cmp)
           if cmp.is_menu_visible() then
@@ -23,7 +23,6 @@ return {
         end,
         "fallback",
       },
-      -- Keep your other mappings
       ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
       ["<C-e>"] = { "hide", "fallback" },
       ["<CR>"] = { "accept", "fallback" },
@@ -33,46 +32,6 @@ return {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = "mono",
     },
-
-    -- sources = {
-    --   -- Add 'obsidian' to the list of default providers
-    --   -- default = { "lsp", "path", "snippets", "buffer", "obsidian" },
-    --
-    --   providers = {
-    --     -- Configure the obsidian provider
-    --     -- obsidian = {
-    --     --   name = "obsidian-blink",
-    --     --   module = "blink.cmp.sources.lib",
-    --     --   fallbacks = { "lsp" },
-    --     --   -- Only show obsidian suggestions in your vault
-    --     --   -- enabled = function()
-    --     --   --   return vim.bo.filetype == "markdown" and require("obsidian").get_client():current_workspace() ~= nil
-    --     --   -- end,
-    --     -- },
-    --   },
-    -- },
-
-    -- this works altough with errors
-    -- sources = {
-    --   -- Add all three obsidian providers to the default list;
-    --   default = { "lsp", "path", "snippets", "buffer", "obsidian", "obsidian_new", "obsidian_tags" },
-    --   providers = {
-    --     obsidian = {
-    --       name = "obsidian",
-    --       module = "obsidian.completion.sources.blink.refs",
-    --       score_offset = 100, -- Prioritize existing notes over "Create New"
-    --     },
-    --     obsidian_new = {
-    --       name = "obsidian_new",
-    --       module = "obsidian.completion.sources.blink.new",
-    --       score_offset = 80, -- Lower priority than existing notes
-    --     },
-    --     obsidian_tags = {
-    --       name = "obsidian_tags",
-    --       module = "obsidian.completion.sources.blink.tags",
-    --     },
-    --   },
-    -- },
 
     -- Optional: Better UI for suggestions
     completion = {
