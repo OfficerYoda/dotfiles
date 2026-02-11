020f06f87f89dc9604192da0c734929138a23b7a
Liam Verzano <l.verzano@sap.com>
2026-02-11 13:43:20 +0100

    Automatic sync: 2026-02-11 13:43:20

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -5,3 +5,10 @@ require("no-status"):setup()
 -- ya pkg add yazi-rs/plugins:full-border
 -- from: https://github.com/yazi-rs/plugins/tree/main/full-border.yazi
 require("full-border"):setup()
+
+-- ya pkg add yazi-rs/plugins:git
+-- from: https://github.com/yazi-rs/plugins/tree/main/git.yazi
+-- require("git"):setup({
+-- 	-- Order of status signs showing in the linemode
+-- 	order = 1500,
+-- })
