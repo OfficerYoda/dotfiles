645d935d51cff4b57d7177695696332253925c05
Liam Verzano <l.verzano@sap.com>
2026-03-03 11:07:07 +0100

    Automatic sync: 2026-03-03 11:07:07

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -9,6 +9,8 @@ return {
         ["]"] = { output = { left = "[[", right = "]]" } },
         ["{"] = { output = { left = "{", right = "}" } },
         ["}"] = { output = { left = "{ ", right = " }" } },
+        ["<"] = { output = { left = "<", right = ">" } },
+        [">"] = { output = { left = "< ", right = " >" } },
       },
     },
   },
