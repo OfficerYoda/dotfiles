ba2123a168be6453d2f9750849a26fd2f150a4da
OfficerYoda <officeryoda@gmail.com>
2026-08-17 17:02:02 +0200

    Automatic sync: 2026-08-17 17:02:02

--- fish/functions/_tmux-session-switcher.fish
+++ fish/functions/_tmux-session-switcher.fish
@@ -1,12 +1,12 @@
 function _tmux-session-switcher
     # 1. --multi: Allows selecting multiple sessions with TAB
-    # 2. --bind 'ctrl-x': Kills selected sessions and reloads the list
+    # 2. --bind 'ctrl-k': Kills selected sessions and reloads the list
     set session (tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf-tmux -p 55%,60% \
         --reverse \
         --multi \
         --preview="" \
-        --header "Enter: Switch | Ctrl-X: Kill Session(s)" \
-        --bind "ctrl-x:execute(tmk {+})+reload(tmux list-sessions -F '#{session_name}')")
+        --header "Enter: Switch | ctrl-k: Kill Session(s)" \
+        --bind "ctrl-k:execute(tmk {+})+reload(tmux list-sessions -F '#{session_name}')")
 
     if test -n "$session"
         if set -q TMUX
